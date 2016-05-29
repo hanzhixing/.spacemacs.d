78446a9793640050119af88edfb8592322c800c1
Zhixing Han <zhixing.han.0409@gmail.com>
2016-05-29 13:28:33 +0800

    enable all feature

--- init.el
+++ init.el
@@ -32,9 +32,9 @@ values."
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
-     ;; spell-checking
+     spell-checking
      syntax-checking
-     ;; version-control
+     version-control
      osx
      php
      html
