24a7f25759bdaf47fa93038759b77aeb80946382
Zhixing Han <zhixing.han.0409@gmail.com>
2016-06-04 02:11:22 +0800

    disable spellchecking and syntac checking

--- init.el
+++ init.el
@@ -32,8 +32,8 @@ values."
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
-     spell-checking
-     syntax-checking
+     ;;spell-checking
+     ;;syntax-checking
      ;;version-control
      osx
      php
